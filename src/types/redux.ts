091956b4dc7af4de65b15ad733832856e09a0c83
Larry Barrios Jr <github@larrybarriosjr.com>
2021-07-20 19:35:39 +0800

    Arrange folder structure

---
+++ src/types/redux.ts
@@ -0,0 +1,7 @@
+import { Action, ThunkAction } from "@reduxjs/toolkit"
+import { store } from "app/store"
+
+/* Redux Types */
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>
